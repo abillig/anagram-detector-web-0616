beb838a905fa3836daf741859ad611590be7c74b
abillig <avrambillig@gmail.com>
2016-06-09 19:39:39 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry' 
+
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    answer=[]
+    array.each do |item|
+      if @word.split("").sort.join("") == item.split("").sort.join("")
+answer << item
+end
+  end
+  answer
+end
+
+
+end
+
